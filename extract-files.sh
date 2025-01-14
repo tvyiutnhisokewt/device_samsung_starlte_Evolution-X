87fb3979442a8975af3688d579a4e7a318924208
Localhorst04 <aaron.laib@protonmail.com>
2025-01-14 16:20:38 +0100

    starlte: patch libkeymaster_helper_vendor.so to use the new libcrypto
    Change-Id: Ic801d0a08bfa65f34916d83f8033d20a0583fb0e

--- extract-files.sh
+++ extract-files.sh
@@ -8,17 +8,19 @@
 
 function blob_fixup() {
     case "${1}" in
-	vendor/lib/hw/audio.primary.exynos9810.so)
-	    "${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so "${2}"
-            "${PATCHELF}" --add-needed libshim_audioparams.so "${2}"
-            sed -i 's/str_parms_get_str/str_parms_get_mod/g' "${2}"
-            ;;
-	vendor/lib*/libwrappergps.so)
-	    "${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so "${2}"
-	    ;;
-        vendor/lib*/libexynosdisplay.so|vendor/lib*/hw/hwcomposer.exynos9820.so|vendor/lib*/sensors.*.so)
-            "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
-            ;;
+        vendor/lib/hw/audio.primary.exynos9810.so)
+                "${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so "${2}"
+                "${PATCHELF}" --add-needed libshim_audioparams.so "${2}"
+                sed -i 's/str_parms_get_str/str_parms_get_mod/g' "${2}"
+                ;;
+        vendor/lib*/libkeymaster_helper_vendor.so)
+                "${PATCHELF}" --replace-needed libcrypto.so libcrypto-tm.so "${2}"
+                "${PATCHELF}" --add-needed libssl-tm.so "${2}"
+                "${PATCHELF}" --add-needed libshim_crypto.so "${2}"
+                ;;
+        vendor/lib*/libwrappergps.so)
+                "${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so "${2}"
+                ;;
     esac
 }
 
@@ -34,4 +36,4 @@ export DEVICE=starlte
 export DEVICE_COMMON=exynos9810-common
 export VENDOR=samsung
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./device/${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
